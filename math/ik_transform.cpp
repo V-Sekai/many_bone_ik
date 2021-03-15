145409f58071fb360024c64943bce406dfbfbb4d
Rafael Martinez <rafaelmtzg@gmail.com>
2021-03-15 14:54:00 -0600

    Added IKTransform class

---
+++ math/ik_transform.cpp
@@ -0,0 +1,235 @@
+/*************************************************************************/
+/*  ik_transform.cpp                                                     */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2019 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2019 Godot Engine contributors (cf. AUTHORS.md)    */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "ik_transform.h"
+
+void IKTransform::_propagate_transform_changed() {
+    for (List<IKTransform *>::Element *E = children.front(); E; E = E->next()) {
+		E->get()->_propagate_transform_changed();
+	}
+
+    dirty |= DIRTY_GLOBAL;
+}
+
+void IKTransform::_update_local_transform() const {
+	local_transform.basis.set_euler_scale(rotation, scale);
+
+	dirty &= ~DIRTY_LOCAL;
+}
+
+void IKTransform::set_translation(const Vector3 &p_translation) {
+    local_transform.origin = p_translation;
+	_propagate_transform_changed();
+}
+
+void IKTransform::set_rotation(const Vector3 &p_euler_rad) {
+	if (dirty & DIRTY_VECTORS) {
+		scale = local_transform.basis.get_scale();
+		dirty &= ~DIRTY_VECTORS;
+	}
+
+	rotation = p_euler_rad;
+	dirty |= DIRTY_LOCAL;
+	_propagate_transform_changed();
+}
+
+void IKTransform::set_rotation_degrees(const Vector3 &p_euler_deg) {
+	set_rotation(p_euler_deg * (Math_PI / 180.0));
+}
+
+void IKTransform::set_scale(const Vector3 &p_scale) {
+	if (dirty & DIRTY_VECTORS) {
+		rotation = local_transform.basis.get_rotation();
+		dirty &= ~DIRTY_VECTORS;
+	}
+
+	scale = p_scale;
+	dirty |= DIRTY_LOCAL;
+	_propagate_transform_changed();
+}
+
+Vector3 IKTransform::get_translation() const {
+	return local_transform.origin;
+}
+
+Vector3 IKTransform::get_rotation() const {
+	if (dirty & DIRTY_VECTORS) {
+		scale = local_transform.basis.get_scale();
+		rotation = local_transform.basis.get_rotation();
+
+		dirty &= ~DIRTY_VECTORS;
+	}
+
+	return rotation;
+}
+
+Vector3 IKTransform::get_rotation_degrees() const {
+	return get_rotation() * (180.0 / Math_PI);
+}
+
+Vector3 IKTransform::get_scale() const {
+	if (dirty & DIRTY_VECTORS) {
+		scale = local_transform.basis.get_scale();
+		rotation = local_transform.basis.get_rotation();
+
+		dirty &= ~DIRTY_VECTORS;
+	}
+
+	return scale;
+}
+
+void IKTransform::set_transform(const Transform &p_transform) {
+	local_transform = p_transform;
+	dirty |= DIRTY_VECTORS;
+	_propagate_transform_changed();
+}
+
+void IKTransform::set_global_transform(const Transform &p_transform) {
+	Transform xform = parent ?
+                parent->get_global_transform().affine_inverse() * p_transform :
+                p_transform;
+	set_transform(xform);
+}
+
+Transform IKTransform::get_transform() const {
+	if (dirty & DIRTY_LOCAL) {
+		_update_local_transform();
+	}
+
+	return local_transform;
+}
+
+Transform IKTransform::get_global_transform() const {
+	if (dirty & DIRTY_GLOBAL) {
+		if (dirty & DIRTY_LOCAL) {
+			_update_local_transform();
+		}
+
+		if (parent) {
+			global_transform = parent->get_global_transform() * local_transform;
+		} else {
+			global_transform = local_transform;
+		}
+
+		if (disable_scale) {
+			global_transform.basis.orthonormalize();
+		}
+
+		dirty &= ~DIRTY_GLOBAL;
+	}
+
+	return global_transform;
+}
+
+void IKTransform::set_disable_scale(bool p_enabled) {
+	disable_scale = p_enabled;
+}
+
+bool IKTransform::is_scale_disabled() const {
+	return disable_scale;
+}
+
+void IKTransform::rotate_object_local(const Vector3 &p_axis, float p_angle) {
+	Transform t = get_transform();
+	t.basis.rotate_local(p_axis, p_angle);
+	set_transform(t);
+}
+
+void IKTransform::rotate(const Vector3 &p_axis, float p_angle) {
+	Transform t = get_transform();
+	t.basis.rotate(p_axis, p_angle);
+	set_transform(t);
+}
+
+void IKTransform::rotate_x(float p_angle) {
+	Transform t = get_transform();
+	t.basis.rotate(Vector3(1, 0, 0), p_angle);
+	set_transform(t);
+}
+
+void IKTransform::rotate_y(float p_angle) {
+	Transform t = get_transform();
+	t.basis.rotate(Vector3(0, 1, 0), p_angle);
+	set_transform(t);
+}
+
+void IKTransform::rotate_z(float p_angle) {
+	Transform t = get_transform();
+	t.basis.rotate(Vector3(0, 0, 1), p_angle);
+	set_transform(t);
+}
+
+void IKTransform::translate(const Vector3 &p_offset) {
+	Transform t = get_transform();
+	t.translate(p_offset);
+	set_transform(t);
+}
+
+void IKTransform::translate_object_local(const Vector3 &p_offset) {
+	Transform t = get_transform();
+
+	Transform s;
+	s.translate(p_offset);
+	set_transform(t * s);
+}
+
+void IKTransform::scale_object_local(const Vector3 &p_scale) {
+	Transform t = get_transform();
+	t.basis.scale_local(p_scale);
+	set_transform(t);
+}
+
+void IKTransform::global_rotate(const Vector3 &p_axis, float p_angle) {
+	Transform t = get_global_transform();
+	t.basis.rotate(p_axis, p_angle);
+	set_global_transform(t);
+}
+
+void IKTransform::global_scale(const Vector3 &p_scale) {
+	Transform t = get_global_transform();
+	t.basis.scale(p_scale);
+	set_global_transform(t);
+}
+
+void IKTransform::global_translate(const Vector3 &p_offset) {
+	Transform t = get_global_transform();
+	t.origin += p_offset;
+	set_global_transform(t);
+}
+
+void IKTransform::orthonormalize() {
+	Transform t = get_transform();
+	t.orthonormalize();
+	set_transform(t);
+}
+
+void IKTransform::set_identity() {
+	set_transform(Transform());
+}
---
+++ math/ik_transform.h
@@ -0,0 +1,100 @@
+/*************************************************************************/
+/*  ik_transform.h                                                       */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2019 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2019 Godot Engine contributors (cf. AUTHORS.md)    */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef IK_TRANSFORM_H
+#define IK_TRANSFORM_H
+
+#include "core/math/transform.h"
+#include "core/math/basis.h"
+#include "core/templates/list.h"
+
+class IKTransform {
+	enum TransformDirty {
+		DIRTY_NONE = 0,
+		DIRTY_VECTORS = 1,
+		DIRTY_LOCAL = 2,
+		DIRTY_GLOBAL = 4
+	};
+
+	mutable Transform global_transform;
+	mutable Transform local_transform;
+	mutable Vector3 rotation;
+	mutable Vector3 scale = Vector3(1, 1, 1);
+
+	mutable int dirty = DIRTY_NONE;
+
+	IKTransform *parent = nullptr;
+	List<IKTransform *> children;
+
+	bool disable_scale = false;
+
+	void _propagate_transform_changed();
+	void _update_local_transform() const;
+
+public:
+	void set_translation(const Vector3 &p_translation);
+	void set_rotation(const Vector3 &p_euler_rad);
+	void set_rotation_degrees(const Vector3 &p_euler_deg);
+	void set_scale(const Vector3 &p_scale);
+	Vector3 get_translation() const;
+	Vector3 get_rotation() const;
+	Vector3 get_rotation_degrees() const;
+	Vector3 get_scale() const;
+
+	void set_transform(const Transform &p_transform);
+	void set_global_transform(const Transform &p_transform);
+	Transform get_transform() const;
+	Transform get_global_transform() const;
+
+	void set_disable_scale(bool p_enabled);
+	bool is_scale_disabled() const;
+
+	void rotate(const Vector3 &p_axis, float p_angle);
+	void rotate_x(float p_angle);
+	void rotate_y(float p_angle);
+	void rotate_z(float p_angle);
+	void translate(const Vector3 &p_offset);
+
+	void rotate_object_local(const Vector3 &p_axis, float p_angle);
+	void scale_object_local(const Vector3 &p_scale);
+	void translate_object_local(const Vector3 &p_offset);
+
+	void global_rotate(const Vector3 &p_axis, float p_angle);
+	void global_scale(const Vector3 &p_scale);
+	void global_translate(const Vector3 &p_offset);
+
+	Vector3 to_local(Vector3 p_global) const;
+	Vector3 to_global(Vector3 p_local) const;
+
+	void orthonormalize();
+	void set_identity();
+};
+
+#endif // IK_TRANSFORM_H
