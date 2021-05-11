233539d9e70e12eca0f237e2e23bc9cfd69e7806
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-05-10 20:04:16 -0700

    Revert "Restore dampening later."
    This reverts commit 314e35efc1104449e729b54558bf67c500ca2cba.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -326,7 +326,6 @@ real_t IKBoneChain::set_optimal_rotation(Ref<IKBone3D> p_for_bone, PackedVector3
 	Vector3 axis;
 	float angle;
 	rot.get_axis_angle(axis, angle);
-	// TODO restore later
 	// float bone_damp = p_for_bone->get_cos_half_dampen();
 
 	// if (!Math::is_equal_approx(p_dampening, -1.0)) {
