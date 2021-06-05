05768df490a8f3c80d3ed6c8ab8efc495442df28
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-05 13:37:22 -0700

    Try less damp.

--- skeleton_modification_3d_ewbik.h
+++ skeleton_modification_3d_ewbik.h
@@ -53,7 +53,7 @@ private:
 
 	// Task
 	int32_t ik_iterations = 15;
-	float default_damp = Math::deg2rad(0.1f);
+	float default_damp = Math::deg2rad(1.0f);
 	int32_t constraint_stabilization_passes = 4;
 
 	void update_segments();
