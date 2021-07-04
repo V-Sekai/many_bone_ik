672e9f2e29738ebe9a489f6919b65676fdb8f81d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-07-03 22:13:12 -0700

    Damp at 1 degree.

--- skeleton_modification_3d_ewbik.h
+++ skeleton_modification_3d_ewbik.h
@@ -53,7 +53,7 @@ private:
 
 	// Task
 	int32_t ik_iterations = 10;
-	float default_damp = Math::deg2rad(5.0f);
+	float default_damp = Math::deg2rad(1.0f);
 	int32_t constraint_stabilization_passes = 2;
 
 	void update_segments();
