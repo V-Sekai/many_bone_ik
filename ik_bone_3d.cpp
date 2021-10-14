43418441fe4cbba1a08b2289ff6f3db4329dde53
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-13 23:04:35 -0700

    Restore ewbik.

--- ik_bone_3d.cpp
+++ ik_bone_3d.cpp
@@ -85,16 +85,17 @@ void IKBone3D::set_rot_delta(const Basis &p_rot) {
 }
 
 void IKBone3D::set_initial_transform(Skeleton3D *p_skeleton) {
-	Transform3D bxform = p_skeleton->get_bone_global_pose(bone_id);
-	set_global_transform(bxform);
+	Transform3D bone_xform = p_skeleton->get_bone_global_pose(bone_id);
+	set_global_transform(bone_xform);
 	if (is_effector()) {
 		effector->update_goal_transform(p_skeleton);
 	}
 }
 
 void IKBone3D::set_skeleton_bone_transform(Skeleton3D *p_skeleton, real_t p_strength) {
-	Transform3D custom = Transform3D(Basis(rot_delta), translation_delta);
-	p_skeleton->set_bone_local_pose_override(bone_id, custom, p_strength, true);
+	Transform3D bone_xform = Transform3D(Basis(rot_delta), translation_delta);
+	bone_xform = p_skeleton->get_bone_rest(bone_id) * bone_xform;
+	p_skeleton->set_bone_local_pose_override(bone_id, bone_xform, p_strength, true);
 }
 
 void IKBone3D::create_effector() {
