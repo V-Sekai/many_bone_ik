6b61ebe279e41154dc7a94624f718b2c62c720cf
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-14 07:38:00 -0700

    Include doctool.

--- config.py
+++ config.py
@@ -3,3 +3,15 @@ def can_build(env, platform) :
 
 def configure(env) :
 	pass
+
+def get_doc_classes():
+    return [
+        "SkeletonModification3DEWBIK",
+        "IKBone3D",
+		"IKEffector3D",
+		"IKBoneChain",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
+	
