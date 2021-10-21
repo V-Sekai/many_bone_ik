d641b6170bd0a5f5b5910f2fb653fe973695d9cd
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-21 11:43:43 -0700

    Restore old setup.

--- ik_bone_3d.cpp
+++ ik_bone_3d.cpp
@@ -85,7 +85,7 @@ void IKBone3D::set_rot_delta(const Basis &p_rot) {
 }
 
 void IKBone3D::set_initial_transform(Skeleton3D *p_skeleton) {
-	Transform3D bxform = p_skeleton->get_bone_global_pose(bone_id);
+	Transform3D bxform = p_skeleton->get_bone_rest(bone_id).affine_inverse() * p_skeleton->get_bone_global_pose(bone_id);
 	set_global_transform(bxform);
 	if (is_effector()) {
 		effector->update_goal_transform(p_skeleton);
@@ -94,6 +94,7 @@ void IKBone3D::set_initial_transform(Skeleton3D *p_skeleton) {
 
 void IKBone3D::set_skeleton_bone_transform(Skeleton3D *p_skeleton, real_t p_strength) {
 	Transform3D custom = Transform3D(Basis(rot_delta), translation_delta);
+	custom = p_skeleton->get_bone_rest(bone_id) * custom;
 	p_skeleton->set_bone_local_pose_override(bone_id, custom, p_strength, true);
 }
 
