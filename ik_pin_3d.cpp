871960b08f2403b82cce35cfee42a3205c31af08
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-04-30 15:00:39 -0700

    Match rotation and location.

--- ik_pin_3d.cpp
+++ ik_pin_3d.cpp
@@ -121,33 +121,31 @@ void IKPin3D::create_headings(const Vector<real_t> &p_weights) {
 void IKPin3D::update_effector_target_headings(PackedVector3Array *p_headings, int32_t &p_index,
 		Vector<real_t> *p_weights) const {
 	ERR_FAIL_NULL(p_headings);
+	float scale_by = goal_global_pose.origin.length();
 	p_headings->write[p_index] = goal_global_pose.origin;
 	p_index++;
 	{
 		real_t w = p_weights->write[p_index];
-		Vector3 axis_aligned = Vector3(1.0f, 0.0f, 0.0f);
-		p_headings->write[p_index] = goal_global_pose.basis.xform(axis_aligned - goal_global_pose.origin);
-		p_headings->write[p_index] *= Vector3(w, 1.0f, 1.0f);
-		p_headings->write[p_index + 1] = goal_global_pose.basis.xform(-axis_aligned - goal_global_pose.origin);
-		p_headings->write[p_index] *= Vector3(w, 1.0f, 1.0f);
+		p_headings->write[p_index] = goal_global_pose.origin + scale_by * goal_global_pose.xform(Vector3(1.0f, 0.0f, 0.0f));
+		p_headings->write[p_index] *= Vector3(w, 1.0, 1.0);
+		p_headings->write[p_index + 1] = goal_global_pose.origin - scale_by * goal_global_pose.xform(Vector3(1.0f, 0.0f, 0.0f));
+		p_headings->write[p_index] *= Vector3(w, 1.0, 1.0);
 		p_index += 2;
 	}
 	{
 		real_t w = p_weights->write[p_index];
-		Vector3 axis_aligned = Vector3(0.0f, 1.0f, 0.0f);
-		p_headings->write[p_index] = goal_global_pose.basis.xform(axis_aligned - goal_global_pose.origin);
+		p_headings->write[p_index] = goal_global_pose.origin + scale_by * goal_global_pose.xform(Vector3(0.0f, 1.0f, 0.0f));
 		p_headings->write[p_index] *= Vector3(1.0, w, 1.0);
-		p_headings->write[p_index + 1] = goal_global_pose.basis.xform(-axis_aligned - goal_global_pose.origin);
-		p_headings->write[p_index + 1] *= Vector3(1.0, w, 1.0);
+		p_headings->write[p_index + 1] = goal_global_pose.origin - scale_by * goal_global_pose.xform(Vector3(0.0f, 1.0f, 0.0f));
+		p_headings->write[p_index] *= Vector3(1.0, w, 1.0);
 		p_index += 2;
 	}
 	{
 		real_t w = p_weights->write[p_index];
-		Vector3 axis_aligned = Vector3(0.0f, 0.0f, 1.0f);
-		p_headings->write[p_index] = goal_global_pose.basis.xform(axis_aligned - goal_global_pose.origin);
+		p_headings->write[p_index] = goal_global_pose.origin + scale_by * goal_global_pose.xform(Vector3(0.0f, 0.0f, 1.0f));
 		p_headings->write[p_index] *= Vector3(1.0, 1.0, w);
-		p_headings->write[p_index + 1] = goal_global_pose.basis.xform(-axis_aligned - goal_global_pose.origin);
-		p_headings->write[p_index + 1] *= Vector3(1.0, 1.0, w);
+		p_headings->write[p_index + 1] = goal_global_pose.origin - scale_by * goal_global_pose.xform(Vector3(0.0f, 0.0f, 1.0f));
+		p_headings->write[p_index] *= Vector3(1.0, 1.0, w);
 		p_index += 2;
 	}
 }
@@ -157,33 +155,25 @@ void IKPin3D::update_effector_tip_headings(Ref<IKBone3D> p_current_bone, PackedV
 	ERR_FAIL_NULL(p_current_bone);
 	Transform3D tip_xform = for_bone->get_global_pose();
 	p_headings->write[p_index] = tip_xform.origin;
+	p_index++;
+	Vector3 v = tip_xform.xform(Vector3());
 	float scale_by = tip_xform.origin.length();
 	if (for_bone->get_parent().is_valid()) {
 		scale_by = for_bone->get_parent()->get_global_pose().origin.distance_to(tip_xform.origin);
 	}
-	p_index++;
 	{
-		Vector3 axis_aligned = tip_xform.origin + Vector3(1.0f, 0.0f, 0.0f);
-		p_headings->write[p_index] = tip_xform.basis.xform(axis_aligned - tip_xform.origin);
-		p_headings->write[p_index] *= Vector3(scale_by, 1.0f, 1.0f);
-		p_headings->write[p_index + 1] = tip_xform.basis.xform(-axis_aligned - tip_xform.origin);
-		p_headings->write[p_index + 1] *= Vector3(scale_by, 1.0f, 1.0f);
+		p_headings->write[p_index] = tip_xform.origin + scale_by * tip_xform.xform(Vector3(1.0f, 0.0f, 0.0f));
+		p_headings->write[p_index + 1] = tip_xform.origin - scale_by * tip_xform.xform(Vector3(1.0f, 0.0f, 0.0f));
 		p_index += 2;
 	}
 	{
-		Vector3 axis_aligned = tip_xform.origin + Vector3(0.0f, 1.0f, 0.0f);
-		p_headings->write[p_index] = tip_xform.basis.xform(axis_aligned - tip_xform.origin);
-		p_headings->write[p_index] *= Vector3(1.0f, scale_by, 1.0f);
-		p_headings->write[p_index + 1] = tip_xform.basis.xform(-axis_aligned - tip_xform.origin);
-		p_headings->write[p_index + 1] *= Vector3(1.0f, scale_by, 1.0f);
+		p_headings->write[p_index] = tip_xform.origin + scale_by * tip_xform.xform(Vector3(0.0f, 1.0f, 0.0f));
+		p_headings->write[p_index + 1] = tip_xform.origin - scale_by * tip_xform.xform(Vector3(0.0f, 1.0f, 0.0f));
 		p_index += 2;
 	}
 	{
-		Vector3 axis_aligned = tip_xform.origin + Vector3(0.0f, 0.0f, 1.0f);
-		p_headings->write[p_index] = tip_xform.basis.xform(axis_aligned - tip_xform.origin);
-		p_headings->write[p_index] *= Vector3(1.0f, 1.0f, scale_by);
-		p_headings->write[p_index + 1] = tip_xform.basis.xform(-axis_aligned - tip_xform.origin);
-		p_headings->write[p_index] *= Vector3(1.0f, 1.0f, scale_by);
+		p_headings->write[p_index] = tip_xform.origin + scale_by * tip_xform.xform(Vector3(0.0f, 0.0f, 1.0f));
+		p_headings->write[p_index + 1] = tip_xform.origin - scale_by * tip_xform.xform(Vector3(0.0f, 0.0f, 1.0f));
 		p_index += 2;
 	}
 }
