e231fc9843407033f7e73b0ec8a4cc76aea600e7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-02 06:46:37 -0700

    Connect renamed signal to the pinned node set.

--- skeleton_modification_3d_ewbik.cpp
+++ skeleton_modification_3d_ewbik.cpp
@@ -112,11 +112,16 @@ void SkeletonModification3DEWBIK::set_pin_target_nodepath(int32_t p_pin_index, c
 	if (!node) {
 		return;
 	}
-	bool is_connected = !node->is_connected(SNAME("tree_entered"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath));
-	if (is_connected) {
+	bool is_tree_entered_connected = !node->is_connected(SNAME("tree_entered"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath));
+	if (is_tree_entered_connected) {
 		node->disconnect(SNAME("tree_entered"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath));
 	}
 	node->connect(SNAME("tree_entered"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath), varray(p_pin_index, node));
+	bool is_renamed_connected = !node->is_connected(SNAME("renamed"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath));
+	if (is_renamed_connected) {
+		node->disconnect(SNAME("renamed"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath));
+	}
+	node->connect(SNAME("renamed"), callable_mp(this, &SkeletonModification3DEWBIK::update_pin_target_nodepath), varray(p_pin_index, node));
 }
 
 void SkeletonModification3DEWBIK::update_pin_target_nodepath(int32_t p_pin_index, Node *p_node) {
