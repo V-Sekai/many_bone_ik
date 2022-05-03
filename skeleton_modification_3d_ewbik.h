98de4af37f4f805a78c05bd74e3a8f420a4926ff
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-03 05:35:33 -0700

    Default to more stability.

--- skeleton_modification_3d_ewbik.h
+++ skeleton_modification_3d_ewbik.h
@@ -113,8 +113,8 @@ private:
 	PackedFloat32Array kusudana_twist;
 	Vector<PackedColorArray> kusudama_limit_cones;
 	float MAX_KUSUDAMA_LIMIT_CONES = 30;
-	int32_t ik_iterations = 2;
-	float default_damp = Math::deg2rad(0.1f);
+	int32_t ik_iterations = 4;
+	float default_damp = Math::deg2rad(0.03f);
 
 	void update_effectors_map();
 	void update_shadow_bones_transform();
