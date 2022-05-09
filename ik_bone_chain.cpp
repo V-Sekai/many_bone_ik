2b12613ddf341c057177177f6fc89aa0cc9828eb
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-09 06:57:41 -0700

    Don't orthogonalize.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -238,7 +238,7 @@ void IKBoneChain::set_optimal_rotation(Ref<IKBone3D> p_for_bone, PackedVector3Ar
 		parent_global_pose_basis = p_for_bone->get_parent()->get_global_pose().basis;
 	}
 	Basis new_rotation = parent_global_pose_basis.inverse() * rot * parent_global_pose_basis;
-	Transform3D result = Transform3D((new_rotation * p_for_bone->get_pose().basis).orthogonalized(), p_for_bone->get_pose().origin);
+	Transform3D result = Transform3D((new_rotation * p_for_bone->get_pose().basis), p_for_bone->get_pose().origin);
 	p_for_bone->set_pose(result);
 	result = Transform3D(p_for_bone->get_global_pose().basis, p_for_bone->get_global_pose().origin + translation);
 	p_for_bone->set_global_pose(result);
