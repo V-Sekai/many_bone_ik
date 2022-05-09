101781f0ecb5cdd2bbfcad384634c0cba2bc7eea
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-09 06:10:23 -0700

    Remove unused math.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -234,10 +234,8 @@ double IKBoneChain::set_optimal_rotation(Ref<IKBone3D> p_for_bone, PackedVector3
 		rot = clamp_to_quadrance_angle(rot, bone_damp);
 	}
 	Basis parent_global_pose_basis = p_for_bone->get_pose().basis;
-	Vector3 parent_global_pose_origin = p_for_bone->get_pose().origin;
 	if (p_for_bone->get_parent().is_valid()) {
 		parent_global_pose_basis = p_for_bone->get_parent()->get_global_pose().basis;
-		parent_global_pose_origin = p_for_bone->get_parent()->get_global_pose().origin;
 	}
 	Basis new_rotation = parent_global_pose_basis.inverse() * rot * parent_global_pose_basis;
 	Transform3D result = Transform3D((new_rotation * p_for_bone->get_pose().basis).orthogonalized(), p_for_bone->get_pose().origin);
