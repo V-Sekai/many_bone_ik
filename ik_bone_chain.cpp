9bf8d1e803f91e5f19d35d814ffa679a70b37151
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-09 15:56:47 -0700

    Add sine sample.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -194,6 +194,18 @@ Quaternion IKBoneChain::clamp_to_angle(Quaternion p_quat, real_t p_angle) const 
 		x += real_t(.225) * x * (std::abs(x) - real_t(1.));
 #endif
 	}
+// 	if (false) {
+// 		// real_t sine(float x)
+// 		constexpr real_t B = 4. / Math_PI;
+// 		constexpr real_t C = -4. / (Math_PI * Math_PI);
+// 		real_t y = B * x + C * x * Math::abs(x);
+// #ifdef EXTRA_PRECISION
+// 		//  const real_t Q = 0.775;
+// 		const real_t P = 0.225;
+
+// 		y = P * (y * Math::abs(y) - y) + y; // Q * y + P * y * Math::abs(y)
+// #endif
+// 	}
 	real_t cos_half_angle = x;
 	return clamp_to_quadrance_angle(p_quat, cos_half_angle);
 }
