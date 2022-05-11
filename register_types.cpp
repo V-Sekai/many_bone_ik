1855f087b6315a776192b5aa5f4310198c7d92f2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-10 23:35:47 -0700

    Copied the skeleton plugin.

--- register_types.cpp
+++ register_types.cpp
@@ -33,15 +33,24 @@
 #include "ik_effector_3d.h"
 #include "skeleton_modification_3d_ewbik.h"
 
+#ifdef TOOLS_ENABLED
+#include "editor/ewbik_skeleton_3d_editor_plugin.h"
+#endif
+
 void initialize_ewbik_module(ModuleInitializationLevel p_level) {
-	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-		return;
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+		ClassDB::register_class<IKEffectorTemplate>();
+		ClassDB::register_class<SkeletonModification3DEWBIK>();
+		ClassDB::register_class<IKBone3D>();
+		ClassDB::register_class<IKEffector3D>();
+		ClassDB::register_class<IKBoneChain>();
+		
 	}
-	ClassDB::register_class<IKEffectorTemplate>();
-	ClassDB::register_class<SkeletonModification3DEWBIK>();
-	ClassDB::register_class<IKBone3D>();
-	ClassDB::register_class<IKEffector3D>();
-	ClassDB::register_class<IKBoneChain>();
+#ifdef TOOLS_ENABLED
+	if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
+		EditorPlugins::add_by_type<EWBIKSkeleton3DEditorPlugin>();
+	}
+#endif
 }
 
 void uninitialize_ewbik_module(ModuleInitializationLevel p_level) {
