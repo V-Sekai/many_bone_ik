7b8be1ae63ed7a42a39957f18bc3de7e2782963c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-14 12:37:41 -0700

    Don't set any default values.

--- editor/ewbik_skeleton_3d_editor_plugin.cpp
+++ editor/ewbik_skeleton_3d_editor_plugin.cpp
@@ -1497,11 +1497,6 @@ void EWBIKSkeleton3DGizmoPlugin::redraw(EditorNode3DGizmo *p_gizmo) {
 			constexpr int32_t KUSUDAMA_MAX_CONES = 30;
 			kusudama_limit_cones.resize(KUSUDAMA_MAX_CONES * 4);
 			kusudama_limit_cones.fill(0.0f);
-			Vector3 origin = Vector3(0.0f, 1.0f, 0.0f);
-			kusudama_limit_cones.write[0] = origin.x;
-			kusudama_limit_cones.write[1] = origin.y;
-			kusudama_limit_cones.write[2] = origin.z;
-			kusudama_limit_cones.write[3] = Math_PI / 4.0f;
 			kusudama_material->set_shader_param("coneSequence", kusudama_limit_cones);
 			kusudama_material->set_shader_param("kusudamaColor", current_bone_color);
 
