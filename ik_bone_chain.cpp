1ff3a5910ca83c20bbb40dd3d123daef91b5c711
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-14 07:56:23 -0700

    Remove old rotate by code.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -239,11 +239,6 @@ void IKBoneChain::set_optimal_rotation(Ref<IKBone3D> p_for_bone, PackedVector3Ar
 	if (p_for_bone->get_parent().is_valid()) {
 		parent_global_pose_basis = p_for_bone->get_parent()->get_global_pose().basis;
 	}
-	/*Quaternion new_rotation = parent_global_pose_basis.get_rotation_quaternion();
-	new_rotation = new_rotation.inverse() * rot * new_rotation;
-	new_rotation.normalize();
-	Transform3D result = Transform3D((new_rotation * p_for_bone->get_pose().basis), p_for_bone->get_pose().origin);
-	p_for_bone->set_pose(result);*/
 	p_for_bone->get_ik_transform()->rotateBy(rot);
 	p_for_bone->getConstraint()->setAxesToSoftOrientationSnap(p_for_bone->get_ik_transform(), p_for_bone->get_ik_transform(), p_for_bone->get_ik_transform(), p_for_bone->get_cos_half_dampen());
 	Transform3D result = Transform3D(p_for_bone->get_global_pose().basis, p_for_bone->get_global_pose().origin + translation);
