004b01c57e704bcfb9e6fe1a5d5b41e8466c71b1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-14 12:36:25 -0700

    Remove test constraint.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -241,8 +241,11 @@ void IKBoneChain::set_optimal_rotation(Ref<IKBone3D> p_for_bone, PackedVector3Ar
 		parent_global_pose_basis = p_for_bone->get_parent()->get_global_pose().basis;
 	}
 	p_for_bone->get_ik_transform()->rotate_by(rot);
-	Quaternion snap_rotation = p_for_bone->getConstraint()->set_axes_to_soft_orientation_snap(p_for_bone->get_ik_transform(), p_for_bone->get_ik_transform(), memnew(IKTransform3D()), p_for_bone->get_cos_half_dampen());	
-	p_for_bone->get_ik_transform()->rotate_by(snap_rotation);
+	Quaternion snap_rotation;
+	if (p_for_bone->getConstraint().is_valid()) {
+		snap_rotation = p_for_bone->getConstraint()->set_axes_to_soft_orientation_snap(p_for_bone->get_ik_transform(), p_for_bone->get_ik_transform(), memnew(IKTransform3D()), p_for_bone->get_cos_half_dampen());
+		p_for_bone->get_ik_transform()->rotate_by(snap_rotation);
+	}
 	Transform3D result = Transform3D(p_for_bone->get_global_pose().basis, p_for_bone->get_global_pose().origin + translation);
 	p_for_bone->set_global_pose(result);
 }
