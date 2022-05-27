9b83b0c07b3b01b40acf646a3e134a0af5f5de0a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-27 08:36:45 -0700

    Don't clamp orientation snaps.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -239,11 +239,6 @@ void IKBoneSegment::set_optimal_rotation(Ref<IKBone3D> p_for_bone, PackedVector3
 	if (p_for_bone->getConstraint().is_valid() && p_for_bone->get_constraint_transform().is_valid()) {
 		if (p_for_bone->getConstraint()->is_orientationally_constrained()) {
 			Quaternion orientation_snap = p_for_bone->getConstraint()->get_axes_to_orientation_snap(p_for_bone->get_ik_transform(), p_for_bone->get_constraint_transform(), p_for_bone->get_cos_half_dampen());
-			if (!Math::is_equal_approx(p_dampening, -1.0f)) {
-				orientation_snap = clamp_to_angle(orientation_snap, p_dampening);
-			} else {
-				orientation_snap = clamp_to_quadrance_angle(orientation_snap, bone_damp);
-			}
 			p_for_bone->get_ik_transform()->rotate_local_with_global(orientation_snap);
 		}
 		if (p_for_bone->getConstraint()->is_axially_constrained()) {
