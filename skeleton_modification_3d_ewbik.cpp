2bd262f15448340d62cdc0c16f6043eac6cb38d9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-06-12 01:35:28 -0700

    Do NOT use physics process.

--- skeleton_modification_3d_ewbik.cpp
+++ skeleton_modification_3d_ewbik.cpp
@@ -661,7 +661,6 @@ void SkeletonModification3DEWBIK::_bind_methods() {
 
 SkeletonModification3DEWBIK::SkeletonModification3DEWBIK() {
 	enabled = true;
-	execution_mode = 1;
 }
 
 SkeletonModification3DEWBIK::~SkeletonModification3DEWBIK() {
