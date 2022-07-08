2aac324e74897c9ef4cbb4dfee77cc77ac430ae9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-07-08 10:35:53 -0700

    Add a msd limit test.

--- ik_bone_chain.cpp
+++ ik_bone_chain.cpp
@@ -169,7 +169,10 @@ void IKBoneSegment::update_optimal_rotation(Ref<IKBone3D> p_for_bone, real_t p_d
 	ERR_FAIL_NULL(p_for_bone);
 	update_target_headings(p_for_bone, &heading_weights, &target_headings);
 	update_tip_headings(p_for_bone, &tip_headings);
-	set_optimal_rotation(p_for_bone, &tip_headings, &target_headings, &heading_weights, p_damp, p_translate);
+	double msd = get_manual_msd(tip_headings, target_headings, heading_weights);
+	if (msd > 0.00001) {
+		set_optimal_rotation(p_for_bone, &tip_headings, &target_headings, &heading_weights, p_damp, p_translate);
+	}
 }
 
 Quaternion IKBoneSegment::set_quadrance_angle(Quaternion p_quat, real_t p_cos_half_angle) const {
