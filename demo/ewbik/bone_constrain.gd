6c7ee1b6a353dfa702fa227eff897c7ed1681d76
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-13 15:11:29 -0800

    Save work.

--- demo/ewbik/bone_constrain.gd
+++ demo/ewbik/bone_constrain.gd
@@ -114,29 +114,27 @@ func _run():
 		ewbik.set_pin_nodepath(pin_i, NodePath(path_string))
 		ewbik.set_pin_nodepath(pin_i, NodePath(path_string))
 		ewbik.set_pin_bone_name(pin_i, bone_name)
-		ewbik.set_pin_depth_falloff(pin_i, 0)
-		ewbik.set_pin_weight(pin_i, 0.5)
+		ewbik.set_pin_depth_falloff(pin_i, 1)
+		ewbik.set_pin_weight(pin_i, 0.2)
 		if bone_name in ["Head"]:
-			ewbik.set_pin_weight(pin_i, 0.2)
+			ewbik.set_pin_weight(pin_i, 1)
+		if bone_name in ["LeftFoot", "RightFoot"]:
+			ewbik.set_pin_weight(pin_i, 0.4)
 
 		var bone_id = skeleton.find_bone(bone_name)
 		if bone_id == -1:
 			pin_i = pin_i + 1
 			continue
 		var bone_global_pose : Transform3D = skeleton.get_bone_global_rest(bone_id)
-		node_3d.global_transform =  skeleton.transform * bone_global_pose
+		node_3d.transform =  skeleton.global_transform * bone_global_pose
 		pin_i = pin_i + 1
 	ewbik.set_constraint_count(human_bone.size())
 	for bone_i in range(human_bone.size()):
 		var bone_name : String = human_bone[bone_i]
-		ewbik.set_constraint_name(bone_i, bone_name)
 		var constraint_i : int = ewbik.find_constraint(bone_name)
 		ewbik.set_constraint_name(constraint_i, bone_name)
-		ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-270),  deg_to_rad(270)))
 #		# https://pubmed.ncbi.nlm.nih.gov/32644411/
-		if bone_name.is_empty() in []:
-			pass
-		elif bone_name in ["Chest"]:
+		if bone_name in ["Chest"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 1)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(10))
@@ -259,3 +257,41 @@ func _run():
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 0, -1))
 			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(15))
 			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(0),  deg_to_rad(130)))
+		elif bone_name in [
+			"LeftEye",
+			"RightEye",
+			"Jaw",
+			"LeftThumbMetacarpal",
+			"LeftThumbProximal",
+			"LeftThumbDistal",
+			"LeftIndexProximal",
+			"LeftIndexIntermediate",
+			"LeftIndexDistal",
+			"LeftMiddleProximal",
+			"LeftMiddleIntermediate",
+			"LeftMiddleDistal",
+			"LeftRingProximal",
+			"LeftRingIntermediate",
+			"LeftRingDistal",
+			"LeftLittleProximal",
+			"LeftLittleIntermediate",
+			"LeftLittleDistal",
+			"RightThumbMetacarpal",
+			"RightThumbProximal",
+			"RightThumbDistal",
+			"RightIndexProximal",
+			"RightIndexIntermediate",
+			"RightIndexDistal",
+			"RightMiddleProximal",
+			"RightMiddleIntermediate",
+			"RightMiddleDistal",
+			"RightRingProximal",
+			"RightRingIntermediate",
+			"RightRingDistal",
+			"RightLittleProximal",
+			"RightLittleIntermediate",
+			"RightLittleDistal"
+			]:
+			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(-270),  deg_to_rad(270)))
+		else:
+			print(bone_name)
