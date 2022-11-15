3dca361bc0e1774812098cdc76ab50a26b33429b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-15 11:20:29 -0800

    Be more loose.

--- demo/n_bone_ik/bone_constrain.gd
+++ demo/n_bone_ik/bone_constrain.gd
@@ -181,7 +181,7 @@ func create_constraints(ewbik):
 		elif bone_name in ["UpperChest"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 1)
 			ewbik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 1, 0))
-			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(30))
+			ewbik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(80))
 			ewbik.set_kusudama_twist(constraint_i, Vector2(deg_to_rad(0), deg_to_rad(360)))
 		elif bone_name in ["Chest"]:
 			ewbik.set_kusudama_limit_cone_count(constraint_i, 1)
