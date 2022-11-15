c2457786f8b842429cd921f1224542b2ec950734
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-15 04:50:03 -0800

    Add gut example.

---
+++ demo/tests/gut_example.gd
@@ -0,0 +1,20 @@
+@tool
+extends GutTest
+
+func before_all():
+	gut.p("Runs once before all tests")
+
+func before_each():
+	gut.p("Runs before each test.")
+
+func after_each():
+	gut.p("Runs after each test.")
+
+func after_all():
+	gut.p("Runs once after all tests")
+
+func test_assert_eq_number_not_equal():
+	assert_eq(1, 2, "Should fail.  1 != 2")
+
+func test_assert_eq_number_equal():
+	assert_eq('asdf', 'asdf', "Should pass")
