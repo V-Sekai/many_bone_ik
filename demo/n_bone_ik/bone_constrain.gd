40154088d932f69ee1116fb8de370fbf97e3d1b8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-20 14:05:40 -0800

    Update HairSample_Female.vrm.import, HairSample_Male.vrm.import, and 2 more files...

--- demo/n_bone_ik/bone_constrain.gd
+++ demo/n_bone_ik/bone_constrain.gd
@@ -18,14 +18,8 @@ func constraint(new_ik : NBoneIK, skeleton : Skeleton3D, root : Node) -> void:
 	var pins =  [
 		skeleton.get_bone_name(skeleton.get_parentless_bones()[0])
 	]
-	for bone_name in ["Head",
-		"LeftHand", 
-		"RightHand", 
-		"LeftFoot", 
-		"RightFoot",
-		"Spine",
-		"Root"]:
-		var bone_i : int = skeleton.find_bone(bone_name)
+	for bone_i in skeleton.get_bone_count():
+		var bone_name = skeleton.get_bone_name(bone_i)
 		if bone_i == -1:
 			continue
 		if pins.has(bone_name):
@@ -35,34 +29,9 @@ func constraint(new_ik : NBoneIK, skeleton : Skeleton3D, root : Node) -> void:
 		var node = root.find_child(pin)
 		if node != null:
 			node.free()
-	new_ik.set_pin_count(pins.size())
 	skeleton.reset_bone_poses()
-	for pin_i in range(pins.size()):
-		var pin = pins[pin_i]
-		var bone_name = pin
-		var bone_i = skeleton.find_bone(bone_name)
-		if bone_i == -1:
-			continue
-		if bone_name in ["Hips"]:
-			new_ik.set_pin_passthrough_factor(pin_i, 0.1)
-		var node_3d : BoneAttachment3D = BoneAttachment3D.new()
-		node_3d.name = bone_name
-		node_3d.bone_name = bone_name
-		node_3d.bone_idx = skeleton.find_bone(bone_name)
-		skeleton.add_child(node_3d)
-		node_3d.owner = root
-		var path_string : String = "../" + str(skeleton.get_path_to(node_3d))
-		new_ik.set_pin_nodepath(pin_i, NodePath(path_string))
-		new_ik.set_pin_bone_name(pin_i, bone_name)
-		new_ik.set_pin_passthrough_factor(pin_i, 1)			
-		if bone_name in ["UpperChest"]:
-			new_ik.set_pin_weight(pin_i, 0.01)
-		var marker_3d : Marker3D = Marker3D.new()
-		marker_3d.name = bone_name
-		marker_3d.global_transform = node_3d.global_transform
-		node_3d.replace_by(marker_3d, true)
-		marker_3d.gizmo_extents = 0.1
-		
+	new_ik.set_pin_count(pins.size())
+	new_ik.set_constraint_count(skeleton.get_bone_count())
 	for constraint_i in skeleton.get_bone_count():
 		var bone_name = skeleton.get_bone_name(constraint_i)
 		var twist_min = new_ik.get_kusudama_twist(constraint_i).x
@@ -149,6 +118,27 @@ func constraint(new_ik : NBoneIK, skeleton : Skeleton3D, root : Node) -> void:
 			new_ik.set_kusudama_limit_cone_center(constraint_i, 0, Vector3(0, 0, -1))
 			new_ik.set_kusudama_limit_cone_radius(constraint_i, 0, deg_to_rad(15))
 			
+	for pin_i in range(pins.size()):
+		var pin = pins[pin_i]
+		var bone_name = pin
+		var bone_i = skeleton.find_bone(bone_name)
+		if bone_i == -1:
+			continue
+		if bone_name in ["Hips"]:
+			new_ik.set_pin_passthrough_factor(pin_i, 0.1)
+		new_ik.set_pin_bone_name(pin_i, bone_name)
+		new_ik.set_pin_passthrough_factor(pin_i, 1)			
+		if bone_name in ["UpperChest"]:
+			new_ik.set_pin_weight(pin_i, 0.01)
+		var marker_3d : Marker3D = Marker3D.new()
+		marker_3d.name = bone_name
+		marker_3d.global_transform = skeleton.get_bone_global_rest(skeleton.find_bone(bone_name))
+		skeleton.add_child(marker_3d)
+		var path_string : String = "../" + str(skeleton.get_path_to(marker_3d))
+		new_ik.set_pin_nodepath(pin_i, NodePath(path_string))
+		marker_3d.owner = root
+		marker_3d.gizmo_extents = 0.1
+		
 func _run():
 	var root : Node3D = get_editor_interface().get_edited_scene_root()
 	if root == null:
