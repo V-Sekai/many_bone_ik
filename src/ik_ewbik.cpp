6ab77d7abd8f87ec1192d1d31f57028ec94c3868
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-20 22:11:10 -0800

    Update ik_ewbik.cpp to use a range of 0 to 360.

--- src/ik_ewbik.cpp
+++ src/ik_ewbik.cpp
@@ -192,7 +192,7 @@ void NBoneIK::_get_property_list(List<PropertyInfo> *p_list) const {
 		p_list->push_back(
 				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_from", PROPERTY_HINT_RANGE, "180,359.999,0.1,radians,or_lesser,or_greater"));
 		p_list->push_back(
-				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_range", PROPERTY_HINT_RANGE, "-180,180.0,0.1,radians,or_lesser,or_greater"));
+				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_range", PROPERTY_HINT_RANGE, "0,360.0,0.1,radians,or_lesser,or_greater"));
 		p_list->push_back(
 				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_current", PROPERTY_HINT_RANGE, "0,1,0.001"));
 		p_list->push_back(
