d04bf8ab748d17ad55c8edbae92ef4b30e69acd4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-22 08:39:13 -0800

    Clamp twist from.

--- src/ik_ewbik.cpp
+++ src/ik_ewbik.cpp
@@ -206,7 +206,7 @@ void NBoneIK::_get_property_list(List<PropertyInfo> *p_list) const {
 		}
 		p_list->push_back(bone_name);
 		p_list->push_back(
-				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_from", PROPERTY_HINT_RANGE, "0,720,0.1,radians,or_lesser,or_greater", constraint_usage));
+				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_from", PROPERTY_HINT_RANGE, "0,360,0.1,radians,or_lesser,or_greater", constraint_usage));
 		p_list->push_back(
 				PropertyInfo(Variant::FLOAT, "constraints/" + itos(constraint_i) + "/twist_range", PROPERTY_HINT_RANGE, "0,360,0.1,radians", constraint_usage));
 		p_list->push_back(
