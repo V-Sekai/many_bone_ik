1064b23a04d6e38e108226d1a13782c96c70f3d0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-22 08:13:56 -0800

    Resolve build error.

--- src/ik_ewbik.cpp
+++ src/ik_ewbik.cpp
@@ -814,7 +814,7 @@ void NBoneIK::skeleton_changed(Skeleton3D *p_skeleton) {
 			constraint->enable_axial_limits();
 			constraint->set_axial_limits(axial_limit.x, axial_limit.y);
 			ik_bone_3d->add_constraint(constraint);
-			constraint->_update_constraint(p_skeleton);
+			constraint->_update_constraint();
 			break;
 		}
 	}
