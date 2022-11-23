abb382365cae92626c57841e812104adafa59dec
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-11-22 16:28:48 -0800

    Don't touch the bone count datastructure by code.

--- src/ik_ewbik.cpp
+++ src/ik_ewbik.cpp
@@ -859,7 +859,6 @@ void NBoneIK::skeleton_changed(Skeleton3D *p_skeleton) {
 	}
 	ERR_FAIL_COND(!root_bone);
 	BoneId root_bone_index = p_skeleton->find_bone(root_bone);
-	set_bone_count(p_skeleton->get_bone_count());
 	BoneId tip_bone_index = p_skeleton->find_bone(tip_bone);
 	segmented_skeleton = Ref<IKBoneSegment>(memnew(IKBoneSegment(p_skeleton, root_bone, pins, nullptr, root_bone_index, tip_bone_index)));
 	segmented_skeleton->get_root()->get_ik_transform()->set_parent(root_transform);
