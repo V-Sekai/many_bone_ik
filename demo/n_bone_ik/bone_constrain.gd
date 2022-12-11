3f2907e1fa908c3b789fcc89170f5f9b38e8939b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-10 23:37:22 -0800

    Use the latest.

--- demo/n_bone_ik/bone_constrain.gd
+++ demo/n_bone_ik/bone_constrain.gd
@@ -17,24 +17,32 @@ func _run():
 
 	new_ik.visible = false
 	skeleton.reset_bone_poses()
-	
+	var humanoid_profile : SkeletonProfileHumanoid = SkeletonProfileHumanoid.new()
+	var humanoid_bones : PackedStringArray = []
+	for bone_i in humanoid_profile.bone_size:
+		var bone_name : String = humanoid_profile.get_bone_name(bone_i)
+		humanoid_bones.push_back(bone_name)
+		
 	for bone_i in skeleton.get_bone_count():
 		var bone_name : String = skeleton.get_bone_name(bone_i)
-		if bone_name == "Joint_05":
-			var node_3d : BoneAttachment3D = BoneAttachment3D.new()
-			node_3d.name = bone_name
-			node_3d.bone_name = bone_name
-			node_3d.bone_idx = bone_i
-			node_3d.set_use_external_skeleton (true)
-			node_3d.set_external_skeleton("../" + str(root.get_path_to(skeleton)))
-			root.add_child(node_3d, true)
-			node_3d.owner = root
-			new_ik.set_pin_enabled(bone_i, true)
-			new_ik.set_pin_nodepath(bone_i, "../" + str(bone_name))
-			var node_global_transform = node_3d.global_transform
-			var marker_3d : Marker3D = Marker3D.new()
-			marker_3d.name = bone_name
-			marker_3d.global_transform = node_global_transform
-			node_3d.replace_by(marker_3d, true)
+		if not humanoid_bones.has(bone_name):
+			continue
+		if not bone_name in ["Root", "Head", "LeftHand", "RightHand", "LeftFoot", "RightFoot"]:
+			continue
+		var node_3d : BoneAttachment3D = BoneAttachment3D.new()
+		node_3d.name = bone_name
+		node_3d.bone_name = bone_name
+		node_3d.bone_idx = bone_i
+		node_3d.set_use_external_skeleton (true)
+		node_3d.set_external_skeleton("../" + str(root.get_path_to(skeleton)))
+		root.add_child(node_3d, true)
+		node_3d.owner = root
+		new_ik.set_pin_enabled(bone_i, true)
+		new_ik.set_pin_nodepath(bone_i, "../" + str(bone_name))
+		var node_global_transform = node_3d.global_transform
+		var marker_3d : Marker3D = Marker3D.new()
+		marker_3d.name = bone_name
+		marker_3d.global_transform = node_global_transform
+		node_3d.replace_by(marker_3d, true)
 
 	new_ik.visible = true
