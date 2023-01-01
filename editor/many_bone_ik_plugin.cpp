2b58f5bcbc8be15f374ba17a54e30a5f528ec626
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-01 12:26:21 -0800

    Restore cone count and inspectors.

--- editor/many_bone_ik_plugin.cpp
+++ editor/many_bone_ik_plugin.cpp
@@ -415,4 +415,16 @@ void ManyBoneIK3DEditor::_value_changed(const String &p_property, Variant p_valu
 	undo_redo->add_undo_property(ik, p_property, ik->get(p_property));
 	undo_redo->add_do_property(ik, p_property, p_value);
 	undo_redo->commit_action();
+	for (int32_t cone_i = 0; cone_i < MAX_KUSUDAMA_CONES; cone_i++) {
+		center_vector3[cone_i]->hide();
+		radius_float[cone_i]->hide();
+	}
+	for (int32_t cone_i = 0; cone_i < ik->get_kusudama_limit_cone_count(selected_bone); cone_i++) {
+		center_vector3[cone_i]->set_object_and_property(ik, vformat("constraints/%d/kusudama_limit_cone/%d/center", selected_bone, cone_i));
+		center_vector3[cone_i]->update_property();
+		center_vector3[cone_i]->show();
+		radius_float[cone_i]->set_object_and_property(ik, vformat("constraints/%d/kusudama_limit_cone/%d/radius", selected_bone, cone_i));
+		radius_float[cone_i]->update_property();
+		radius_float[cone_i]->show();
+	}
 }
