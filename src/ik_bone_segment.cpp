3bdc721b73f813d6c43b3e694ace87f8c744a318
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-31 16:08:28 -0800

    Avoid double normalization.

--- src/ik_bone_segment.cpp
+++ src/ik_bone_segment.cpp
@@ -165,18 +165,12 @@ Quaternion IKBoneSegment::set_quadrance_angle(Quaternion p_quat, real_t p_cos_ha
 }
 
 Quaternion IKBoneSegment::clamp_to_angle(Quaternion p_quat, real_t p_angle) {
-#ifdef MATH_CHECKS
-	ERR_FAIL_COND_V_MSG(!p_quat.is_normalized(), Quaternion(), "The clamp_to_quadrance_angle input quaternion must be normalized.");
-#endif
 	real_t x = cos(p_angle);
 	real_t cos_half_angle = x;
 	return clamp_to_quadrance_angle(p_quat, cos_half_angle);
 }
 
 Quaternion IKBoneSegment::clamp_to_quadrance_angle(Quaternion p_quat, real_t p_cos_half_angle) {
-#ifdef MATH_CHECKS
-	ERR_FAIL_COND_V_MSG(!p_quat.is_normalized(), Quaternion(), "The clamp_to_quadrance_angle input quaternion must be normalized.");
-#endif
 	real_t newCoeff = real_t(1.0) - (p_cos_half_angle * Math::abs(p_cos_half_angle));
 	Quaternion rot = p_quat;
 	real_t currentCoeff = rot.x * rot.x + rot.y * rot.y + rot.z * rot.z;
