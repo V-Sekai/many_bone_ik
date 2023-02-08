90eedc411318b758b492bc9bbf60f4008726bc8d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-08 09:34:20 -0800

    Default to 5 degrees of movement.

--- src/many_bone_ik_3d.h
+++ src/many_bone_ik_3d.h
@@ -62,7 +62,7 @@ private:
 	Vector<int> kusudama_limit_cone_count;
 	float MAX_KUSUDAMA_LIMIT_CONES = 10;
 	int32_t iterations_per_frame = 20;
-	float default_damp = -1.0f;
+	float default_damp = Math::deg_to_rad(5.0f);
 	bool queue_debug_skeleton = false;
 	Ref<IKNode3D> godot_skeleton_transform = Ref<IKNode3D>(memnew(IKNode3D));
 	Transform3D godot_skeleton_transform_inverse;
