795dc60e768abab3cd7b05a03f2e579e65b8bd7f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-05-14 17:21:51 -0700

    Add BoneState.

--- sample/ewbik/bone_state.gd
+++ sample/ewbik/bone_state.gd
@@ -3,158 +3,149 @@ extends Object
 
 class_name BoneState
 
-func _init(id, transform_id, parent_id, target_id, constraint_id, stiffness):
-	pass
 
-String id = null;
-String parent_id = null;
-String transform_id = null;
-String target_id = null;
-String constraint_id = null;
-private double stiffness = 0.0;
-private HashMap<String, Integer> childMap = new HashMap<>();
-private int index;
-private int parentIdx = -1;
-private int[] childIndices = null;
-private int transformIdx = -1;
-private int constraintIdx = -1;
-private int targetIdx = -1;
+var id: String = null
+var parent_id: String = null
+var transform_id: String = null
+var target_id: String = null
+var constraint_id: String = null
+var stiffness: float = 0.0
+var child_map: Dictionary[String, int] = {}
+var index: int
+var parent_idx: int = -1
+var child_indices: Array[int] = []
+var transform_idx: int = -1
+var constraint_idx: int = -1
+var target_idx: int = -1
 
-private BoneState(String id, String transform_id, String parent_id, String target_id, String constraint_id,  double stiffness) {
-	this.id = id;
-	this.parent_id = parent_id;
-	this.transform_id  = transform_id;
-	this.target_id = target_id; 
-	this.constraint_id = constraint_id; 
-	this.stiffness = stiffness;
-}		
-public TransformState getTransform() {
-	return transforms[this.transformIdx];
-}
-public TargetState getTarget() {
-	if(this.targetIdx ==-1) return null;
-	else return targets[this.targetIdx];
-}
-public double getStiffness() {
-	return stiffness;
-}
-public BoneState getParent() {
-	if(this.parentIdx >= 0)
-		return bones[this.parentIdx];
-	return null;
-}
-public BoneState getChild(String id) {
-	return bones[this.childMap.get(id)];
-}
-public BoneState getChild(int index) {
-	return bones[this.childIndices[index]];
-}
-public void clearChildList() {
-	this.childMap.clear();
-	this.childIndices = new int[] {};
-}
-public int getChildCount() {
-	return childIndices.length;
-}
+func _init(_id: String, _transform_id: String, _parent_id: String, _target_id: String, _constraint_id: String, _stiffness: float) -> void:
+	id = _id
+	parent_id = _parent_id
+	transform_id = _transform_id
+	target_id = _target_id
+	constraint_id = _constraint_id
+	stiffness = _stiffness
 
-private int getTempChildCount() {
-	return this.childMap.values().size();
-}
-public ConstraintState getConstraint() {
-	if(this.constraintIdx >= 0)
-		return constraints[this.constraintIdx];
-	return null;
-}
+func get_transform() -> TransformState:
+	return transforms[transform_idx]
 
-private void prune() {
-	bonesList.set(this.index, null);
-	boneMap.remove(this.id);
-	this.getTransform().prune();
-	if(this.getConstraint() != null) this.getConstraint().prune();
-	if(this.parent_id != null) {
-		boneMap.get(this.parent_id).childMap.remove(this.id);
-	}
-	if(rootBoneState == this)
-		rootBoneState = null;
-}		
-private void setIndex(int index) {
-	this.index = index;
-	if(this.parent_id != null) {
-		BoneState parentBone = boneMap.get(this.parent_id);
-		parentBone.addChild(this.id, this.index);
-	}
-}
-private void  addChild(String id, int childIndex) {
-	this.childMap.put(id, childIndex);
-}
-private void optimize() {
-	Integer[] tempChildren = childMap.values().toArray(new Integer[0]);
-	this.childIndices = new int[tempChildren.length];
-	int j = 0;
-	for(int i=0; i<tempChildren.length; i++) childIndices[i] = tempChildren[i];
-	if(this.parent_id != null)
-		this.parentIdx = boneMap.get(this.parent_id).index;
-	this.transformIdx = transformMap.get(this.transform_id).getIndex();
-	if(this.constraint_id != null)
-		this.constraintIdx = constraintMap.get(this.constraint_id).getIndex();
-	if(this.target_id != null)
-		this.targetIdx = targetMap.get(this.target_id).getIndex();
-}
+func get_target() -> TargetState:
+	if target_idx == -1:
+		return null
+	else:
+		return targets[target_idx]
 
-private void validate() {
-	if(assumeValid) return;
-	TransformState transform = transformMap.get(this.transform_id); 
-	if(transform == null) //check that the bone has a transform
-		throw new RuntimeException("Bone '"+this.id+"' references transform with id '"+this.transform_id+"', but '"+this.transform_id+"' has not been registered with the SkeletonState.");
-	if(this.parent_id != null) { //if this isn't a root bone, ensure the following
-		BoneState parent = boneMap.get(this.parent_id);
-		if(parent == null) //check that the bone listed as its parent has been registered.
-			throw new RuntimeException("Bone '"+this.id+"' references parent bone with id '"+this.parent_id+"', but '"+this.parent_id+"' has not been registered with the SkeletonState.");
-		TransformState parentBonesTransform = transformMap.get(parent.transform_id);
-		TransformState transformsParent = transformMap.get(transform.parent_id);
-		if(parentBonesTransform != transformsParent) { //check that the parent transform of this bones transform 
-			// is the same as the transform as the ransform of the bone's parent
-			throw new RuntimeException("Bone '"+this.id+"' has listed bone with id '"+this.parent_id+"' as its parent, which has a transform_id of '"+parent.transform_id+
-					"' but the parent transform of this bone's transform is listed as "+transform.parent_id+"'. A bone's transform must have the parent bone's transform as its parent");
-		}
-		//avoid grandfather paradoxes
-		BoneState ancestor = parent;
-		while(ancestor != null) {
-			if(ancestor == this) {
-				throw new RuntimeException("Bone '"+this.id+"' is listed as being both a descendant and an ancestor of itself.");
-			}
-			if(ancestor.parent_id == null) 
-				ancestor = null;
-			else {
-				BoneState curr = ancestor;
-				ancestor = boneMap.get(ancestor.parent_id);
-				if(ancestor == null) {
-					throw new RuntimeException("bone with id `"+curr.id+"` lists its parent bone as having id `"+curr.parent_id+"', but no such bone has been registered with the SkeletonState");  
-				}
-			}
-		}
-	} else {
-		if(this.constraint_id != null) {
-			throw new RuntimeException("Bone '"+this.id+"' has been determined to be a root bone. However, root bones may not be constrained."
-					+ "If you wish to constrain the root bone anyway, please insert a fake unconstrained root bone prior to this bone. Give that bone's transform values equal to this bone's, and set this "
-					+ "bone's transformsto identity. ");
-		}
-	}	      		
-	if(this.constraint_id != null) { //if this bone has a constraint, ensure the following:
-		ConstraintState constraint = constraintMap.get(this.constraint_id) ;
-		if(constraint == null)//check that the constraint has been registered
-			throw new RuntimeException("Bone '"+this.id+"' claims to be constrained by '"+this.constraint_id+"', but no such constraint has been registered with this SkeletonState");
-		if(!constraint.forBone_id.equals(this.id)) {
-			throw new RuntimeException("Bone '"+this.id+"' claims to be constrained by '"+constraint.id+"', but constraint of id '"+constraint.id+"' claims to be constraining bone with id '"+constraint.forBone_id+"'");
-		}
-	}
-}
-public int getIndex() {
-	return index;
-}
-public String getIdString() {
-	return this.id;
-}
-public void setStiffness(double stiffness) {
-	this.stiffness = stiffness;
-}
+func get_stiffness() -> float:
+	return stiffness
+
+func get_parent() -> BoneState:
+	if parent_idx >= 0:
+		return bones[parent_idx]
+	return null
+
+func get_child(_id: String) -> BoneState:
+	return bones[child_map[_id]]
+
+func get_child(index: int) -> BoneState:
+	return bones[child_indices[index]]
+
+func clear_child_list() -> void:
+	child_map.clear()
+	child_indices = []
+
+func get_child_count() -> int:
+	return len(child_indices)
+
+func get_temp_child_count() -> int:
+	return len(child_map.values())
+
+func get_constraint() -> ConstraintState:
+	if constraint_idx >= 0:
+		return constraints[constraint_idx]
+	return null
+
+func prune() -> void:
+	bones_list[index] = null
+	bone_map.erase(id)
+	get_transform().prune()
+	if get_constraint() != null:
+		get_constraint().prune()
+	if parent_id != null:
+		bone_map[parent_id].child_map.erase(id)
+	if root_bone_state == self:
+		root_bone_state = null
+
+func set_index(_index: int) -> void:
+	index = _index
+	if parent_id != null:
+		var parent_bone: BoneState = bone_map[parent_id]
+		parent_bone.add_child(id, index)
+
+func add_child(_id: String, child_index: int) -> void:
+	child_map[_id] = child_index
+
+func optimize() -> void:
+	var temp_children: Array[int] = child_map.values()
+	child_indices = temp_children.duplicate()
+	if parent_id != null:
+		parent_idx = bone_map[parent_id].index
+	transform_idx = transform_map[transform_id].get_index()
+	if constraint_id != null:
+		constraint_idx = constraint_map[constraint_id].get_index()
+	if target_id != null:
+		target_idx = target_map[target_id].get_index()
+
+func validate() -> void:
+	if assumeValid:
+		return
+
+	var transform: TransformState = transformMap.get(this.transform_id)
+	if transform == null:
+		raise("Bone '%s' references transform with id '%s', but '%s' has not been registered with the SkeletonState." % [this.id, this.transform_id, this.transform_id])
+
+	if this.parent_id != null:
+		var parent: BoneState = boneMap.get(this.parent_id)
+		if parent == null:
+			raise("Bone '%s' references parent bone with id '%s', but '%s' has not been registered with the SkeletonState." % [this.id, this.parent_id, this.parent_id])
+
+		var parentBonesTransform: TransformState = transformMap.get(parent.transform_id)
+		var transformsParent: TransformState = transformMap.get(transform.parent_id)
+
+		if parentBonesTransform != transformsParent:
+			raise("Bone '%s' has listed bone with id '%s' as its parent, which has a transform_id of '%s' but the parent transform of this bone's transform is listed as %s. A bone's transform must have the parent bone's transform as its parent" % [this.id, this.parent_id, parent.transform_id, transform.parent_id])
+
+		var ancestor: BoneState = parent
+		while ancestor != null:
+			if ancestor == self:
+				raise("Bone '%s' is listed as being both a descendant and an ancestor of itself." % this.id)
+
+			if ancestor.parent_id == null:
+				ancestor = null
+			else:
+				var curr: BoneState = ancestor
+				ancestor = boneMap.get(ancestor.parent_id)
+				if ancestor == null:
+					raise("bone with id `%s` lists its parent bone as having id `%s`, but no such bone has been registered with the SkeletonState" % [curr.id, curr.parent_id])
+
+	else:
+		if this.constraint_id != null:
+			raise("Bone '%s' has been determined to be a root bone. However, root bones may not be constrained. If you wish to constrain the root bone anyway, please insert a fake unconstrained root bone prior to this bone. Give that bone's transform values equal to this bone's, and set this bone's transforms to identity." % this.id)
+
+	if this.constraint_id != null:
+		var constraint: ConstraintState = constraintMap.get(this.constraint_id)
+		if constraint == null:
+			raise("Bone '%s' claims to be constrained by '%s', but no such constraint has been registered with this SkeletonState" % [this.id, this.constraint_id])
+
+		if not constraint.forBone_id == this.id:
+			raise("Bone '%s' claims to be constrained by '%s', but constraint of id '%s' claims to be constraining bone with id '%s'" % [this.id, constraint.id, constraint.id, constraint.forBone_id])
+
+func get_index() -> int:
+	return index
+
+
+func get_id_string() -> String:
+	return this.id
+
+
+func set_stiffness(stiffness: float) -> void:
+	this.stiffness = stiffness
