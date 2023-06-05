540b9a6523b3d8b0296f44cb3366a687f0d009e3
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-05 07:39:13 -0700

    Optimize QCP::calculate_rotation()

--- src/math/qcp.cpp
+++ src/math/qcp.cpp
@@ -74,26 +74,26 @@ Quaternion QCP::calculate_rotation() {
 		Vector3 u = moved[0];
 		Vector3 v = target[0];
 		double norm_product = u.length() * v.length();
+
 		if (norm_product == 0.0) {
 			return Quaternion();
 		}
+
 		double dot = u.dot(v);
+
 		if (dot < ((2.0e-15 - 1.0) * norm_product)) {
 			// The special case: u = -v,
 			// We select a PI angle rotation around
 			// an arbitrary vector orthogonal to u.
 			Vector3 w = u.normalized();
-			return Quaternion(-1 * -w.x, -1 * -w.y, -1 * -w.z, 0.0f).normalized();
+			return Quaternion(-w.x, -w.y, -w.z, 0.0f).normalized();
 		}
 		// The general case: (u, v) defines a plane, we select
 		// the shortest possible rotation: axis orthogonal to this plane.
 		double q0 = Math::sqrt(0.5 * (1.0 + dot / norm_product));
 		double coeff = 1.0 / (2.0 * q0 * norm_product);
 		Vector3 q = v.cross(u);
-		double q1 = coeff * q.x;
-		double q2 = coeff * q.y;
-		double q3 = coeff * q.z;
-		return Quaternion(-1 * q1, -1 * q2, -1 * q3, q0).normalized();
+		return Quaternion(-coeff * q.x, -coeff * q.y, -coeff * q.z, q0).normalized();
 	} else {
 		double a11 = SxxpSyy + Szz - mxEigenV;
 		double a12 = SyzmSzy;
@@ -111,12 +111,14 @@ Quaternion QCP::calculate_rotation() {
 		double a42 = a24;
 		double a43 = a34;
 		double a44 = Szz - SxxpSyy - mxEigenV;
+
 		double a3344_4334 = a33 * a44 - a43 * a34;
 		double a3244_4234 = a32 * a44 - a42 * a34;
 		double a3243_4233 = a32 * a43 - a42 * a33;
 		double a3143_4133 = a31 * a43 - a41 * a33;
 		double a3144_4134 = a31 * a44 - a41 * a34;
 		double a3142_4132 = a31 * a42 - a41 * a32;
+
 		double q1 = a22 * a3344_4334 - a23 * a3244_4234 + a24 * a3243_4233;
 		double q2 = -a21 * a3344_4334 + a23 * a3144_4134 - a24 * a3143_4133;
 		double q3 = a21 * a3244_4234 - a22 * a3144_4134 + a24 * a3142_4132;
@@ -124,46 +126,10 @@ Quaternion QCP::calculate_rotation() {
 
 		double qsqr = q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4;
 
-		/*
-		 * The following code tries to calculate another column in the adjoint matrix
-		 * when the norm of the current column is too small. Usually this commented
-		 * block will never be activated. To be absolutely safe this should be
-		 * uncommented, but it is most likely unnecessary.
-		 */
 		if (qsqr < evec_prec) {
-			q1 = a12 * a3344_4334 - a13 * a3244_4234 + a14 * a3243_4233;
-			q2 = -a11 * a3344_4334 + a13 * a3144_4134 - a14 * a3143_4133;
-			q3 = a11 * a3244_4234 - a12 * a3144_4134 + a14 * a3142_4132;
-			q4 = -a11 * a3243_4233 + a12 * a3143_4133 - a13 * a3142_4132;
-			qsqr = q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4;
-
-			if (qsqr < evec_prec) {
-				double a1324_1423 = a13 * a24 - a14 * a23, a1224_1422 = a12 * a24 - a14 * a22;
-				double a1223_1322 = a12 * a23 - a13 * a22, a1124_1421 = a11 * a24 - a14 * a21;
-				double a1123_1321 = a11 * a23 - a13 * a21, a1122_1221 = a11 * a22 - a12 * a21;
-
-				q1 = a42 * a1324_1423 - a43 * a1224_1422 + a44 * a1223_1322;
-				q2 = -a41 * a1324_1423 + a43 * a1124_1421 - a44 * a1123_1321;
-				q3 = a41 * a1224_1422 - a42 * a1124_1421 + a44 * a1122_1221;
-				q4 = -a41 * a1223_1322 + a42 * a1123_1321 - a43 * a1122_1221;
-				qsqr = q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4;
-
-				if (qsqr < evec_prec) {
-					q1 = a32 * a1324_1423 - a33 * a1224_1422 + a34 * a1223_1322;
-					q2 = -a31 * a1324_1423 + a33 * a1124_1421 - a34 * a1123_1321;
-					q3 = a31 * a1224_1422 - a32 * a1124_1421 + a34 * a1122_1221;
-					q4 = -a31 * a1223_1322 + a32 * a1123_1321 - a33 * a1122_1221;
-					qsqr = q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4;
-
-					if (qsqr < evec_prec) {
-						/*
-						 * if qsqr is still too small, return the identity rotation
-						 */
-						return Quaternion();
-					}
-				}
-			}
+			return Quaternion();
 		}
+
 		q2 *= -1;
 		q3 *= -1;
 		q4 *= -1;
@@ -175,6 +141,7 @@ Quaternion QCP::calculate_rotation() {
 		q2 /= min;
 		q3 /= min;
 		q4 /= min;
+
 		return Quaternion(q2, q3, q4, q1).normalized();
 	}
 }
@@ -217,7 +184,6 @@ Vector3 QCP::move_to_weighted_center(PackedVector3Array &r_to_center, Vector<rea
 	return center;
 }
 
-
 void QCP::inner_product(PackedVector3Array &coords1, PackedVector3Array &coords2) {
 	double x1, x2, y1, y2, z1, z2;
 	double g1 = 0, g2 = 0;
@@ -232,49 +198,41 @@ void QCP::inner_product(PackedVector3Array &coords1, PackedVector3Array &coords2
 	Szy = 0;
 	Szz = 0;
 
-	if (!weight.is_empty()) {
-		for (int i = 0; i < coords1.size(); i++) {
+	bool weight_is_empty = weight.is_empty();
+	int size = coords1.size();
+
+	for (int i = 0; i < size; i++) {
+		if (!weight_is_empty) {
 			x1 = weight[i] * coords1[i].x;
 			y1 = weight[i] * coords1[i].y;
 			z1 = weight[i] * coords1[i].z;
 
 			g1 += x1 * coords1[i].x + y1 * coords1[i].y + z1 * coords1[i].z;
+		} else {
+			x1 = coords1[i].x;
+			y1 = coords1[i].y;
+			z1 = coords1[i].z;
 
-			x2 = coords2[i].x;
-			y2 = coords2[i].y;
-			z2 = coords2[i].z;
-
-			g2 += weight[i] * (x2 * x2 + y2 * y2 + z2 * z2);
-
-			Sxx += (x1 * x2);
-			Sxy += (x1 * y2);
-			Sxz += (x1 * z2);
-
-			Syx += (y1 * x2);
-			Syy += (y1 * y2);
-			Syz += (y1 * z2);
-
-			Szx += (z1 * x2);
-			Szy += (z1 * y2);
-			Szz += (z1 * z2);
+			g1 += x1 * x1 + y1 * y1 + z1 * z1;
 		}
-	} else {
-		for (int i = 0; i < coords1.size(); i++) {
-			g1 += coords1[i].x * coords1[i].x + coords1[i].y * coords1[i].y + coords1[i].z * coords1[i].z;
-			g2 += coords2[i].x * coords2[i].x + coords2[i].y * coords2[i].y + coords2[i].z * coords2[i].z;
 
-			Sxx += coords1[i].x * coords2[i].x;
-			Sxy += coords1[i].x * coords2[i].y;
-			Sxz += coords1[i].x * coords2[i].z;
+		x2 = coords2[i].x;
+		y2 = coords2[i].y;
+		z2 = coords2[i].z;
 
-			Syx += coords1[i].y * coords2[i].x;
-			Syy += coords1[i].y * coords2[i].y;
-			Syz += coords1[i].y * coords2[i].z;
+		g2 += weight_is_empty ? (x2 * x2 + y2 * y2 + z2 * z2) : (weight[i] * (x2 * x2 + y2 * y2 + z2 * z2));
 
-			Szx += coords1[i].z * coords2[i].x;
-			Szy += coords1[i].z * coords2[i].y;
-			Szz += coords1[i].z * coords2[i].z;
-		}
+		Sxx += (x1 * x2);
+		Sxy += (x1 * y2);
+		Sxz += (x1 * z2);
+
+		Syx += (y1 * x2);
+		Syy += (y1 * y2);
+		Syz += (y1 * z2);
+
+		Szx += (z1 * x2);
+		Szy += (z1 * y2);
+		Szz += (z1 * z2);
 	}
 
 	e0 = (g1 + g2) * 0.5;
