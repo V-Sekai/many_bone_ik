dbb39fd7ccd06145c1a9c05c859e00c63a4c2479
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-25 07:36:11 -0700

    Remove nearby pins for axially constrained bones.

--- editor/many_bone_ik_3d_handle_gizmo_plugin.cpp
+++ editor/many_bone_ik_3d_handle_gizmo_plugin.cpp
@@ -114,32 +114,11 @@ void ManyBoneIK3DHandleGizmoPlugin::redraw(EditorNode3DGizmo *p_gizmo) {
 				if (ik_bone.is_null()) {
 					continue;
 				}
-				// TODO Find nearby pins for usability improvement.
-				int32_t pin_i = many_bone_ik->find_constraint(ik_bone->get_name());
-				if (pin_i == -1) {
+				if (!ik_bone->is_axially_constrained()) {
 					continue;
 				}
-				String node_path = many_bone_ik->get_pin_nodepath(pin_i);
-				if (node_path.is_empty()) {
-					continue;
-				}
-				Node *pin_node = many_bone_ik->get_node(node_path);
-
-				BoneId parent_idx = many_bone_ik_skeleton->get_bone_parent(bone_i);
-				BoneId bone_idx = bone_i;
-				bool parent_in_path = false;
-				while (bone_idx != -1) {
-					if (bone_idx == parent_idx) {
-						parent_in_path = true;
-						break;
-					}
-					bone_idx = many_bone_ik_skeleton->get_bone_parent(bone_idx);
-				}
-
-				if ((cast_to<Node>(node_3d) == pin_node && ik_bone->is_axially_constrained()) || parent_in_path) {
-					create_gizmo_handles(bone_i, ik_bone, p_gizmo, current_bone_color, many_bone_ik_skeleton, many_bone_ik);
-					create_twist_gizmo_handles(bone_i, ik_bone, p_gizmo, current_bone_color, many_bone_ik_skeleton, many_bone_ik);
-				}
+				create_gizmo_handles(bone_i, ik_bone, p_gizmo, current_bone_color, many_bone_ik_skeleton, many_bone_ik);
+				create_twist_gizmo_handles(bone_i, ik_bone, p_gizmo, current_bone_color, many_bone_ik_skeleton, many_bone_ik);
 			}
 		}
 	}
