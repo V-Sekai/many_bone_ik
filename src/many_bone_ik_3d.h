0759d01ff1302c0ee23199493dae3e076420a2ae
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-10-02 09:07:17 -0700

    Refactor IKRay3D class and ManyBoneIK3D class
    - Removed get_scaled_projection method from IKRay3D class
    - Added set_state and get_state methods to ManyBoneIK3D class

--- src/many_bone_ik_3d.h
+++ src/many_bone_ik_3d.h
@@ -49,6 +49,11 @@
 #include "editor/editor_undo_redo_manager.h"
 #endif
 
+class ManyBoneIK3DState : public Resource {
+	GDCLASS(ManyBoneIK3DState, Resource);
+
+};
+
 class ManyBoneIK3D : public Node3D {
 	GDCLASS(ManyBoneIK3D, Node3D);
 
@@ -78,6 +83,7 @@ private:
 	int32_t ui_selected_bone = -1, stabilize_passes = 4;
 	bool is_gizmo_dirty = false;
 	Ref<SceneTreeTimer> timer;
+	Ref<ManyBoneIK3DState> ik_state = memnew(ManyBoneIK3DState);
 	void _on_timer_timeout();
 	void update_ik_bones_transform();
 	void update_skeleton_bones_transform();
@@ -99,6 +105,8 @@ protected:
 
 public:
 	void set_pin_bone_name(int32_t p_effector_index, StringName p_name) const;
+	void set_state(Ref<ManyBoneIK3DState> p_state);
+	Ref<ManyBoneIK3DState> get_state() const;
 	static Vector3 convert_attitude_azimuth_to_coordinate(float attitude, float azimuth);
 	static Vector2 convert_coordinate_to_attitude_azimuth(Vector3 p_center);
 	void add_constraint();
