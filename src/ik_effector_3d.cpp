1d96861cdea248a017d00ee033e5429c4d5064fb
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-11-04 15:31:29 -0700

    Fix double parent transform bug.
    - Update the calculation of target_relative_to_skeleton_origin to use the global transform of the skeleton instead of p_many_bone_ik's godot_skeleton_transform_inverse.
    - If current_target_node is not visible or does not exist, set target_relative_to_skeleton_origin to p_skeleton's global transform affine inverse multiplied by for_bone's IK transform global transform.
    - Otherwise, set target_relative_to_skeleton_origin to p_skeleton's global transform affine inverse multiplied by current_target_node's global transform.

--- src/ik_effector_3d.cpp
+++ src/ik_effector_3d.cpp
@@ -79,10 +79,10 @@ void IKEffector3D::update_target_global_transform(Skeleton3D *p_skeleton, ManyBo
 	ERR_FAIL_NULL(for_bone);
 	Node3D *current_target_node = cast_to<Node3D>(p_many_bone_ik->get_node_or_null(target_node_path));
 	if (!current_target_node || !current_target_node->is_visible_in_tree()) {
-		target_relative_to_skeleton_origin = p_many_bone_ik->get_godot_skeleton_transform_inverse() * for_bone->get_ik_transform()->get_global_transform();
+		target_relative_to_skeleton_origin = p_skeleton->get_global_transform().affine_inverse() * for_bone->get_ik_transform()->get_global_transform();
 		return;
 	}
-	target_relative_to_skeleton_origin = current_target_node->get_global_transform();
+	target_relative_to_skeleton_origin = p_skeleton->get_global_transform().affine_inverse() * current_target_node->get_global_transform();
 }
 
 Transform3D IKEffector3D::get_target_global_transform() const {
