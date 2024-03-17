acfac769e60b18eeed78e321e2e8fe0bfc362667
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-03-17 07:51:47 -0700

    Update IK limit cones' control points to restored.

--- src/ik_kusudama_3d.cpp
+++ src/ik_kusudama_3d.cpp
@@ -62,6 +62,11 @@ void IKKusudama3D::_update_constraint(Ref<IKNode3D> p_limiting_axes) {
 	Transform3D new_y_ray = Transform3D(Basis(), new_y);
 	Quaternion old_y_to_new_y = Quaternion(p_limiting_axes->get_global_transform().get_basis().get_column(Vector3::AXIS_Y).normalized(), p_limiting_axes->get_global_transform().get_basis().xform(new_y_ray.origin).normalized());
 	p_limiting_axes->rotate_local_with_global(old_y_to_new_y);
+	for (Ref<IKLimitCone3D> limit_cone : limit_cones) {
+		Vector3 transformed_control_point = p_limiting_axes->get_global_transform().basis.xform_inv(limit_cone->get_control_point());
+		transformed_control_point = p_limiting_axes->get_global_transform().basis.xform(transformed_control_point);
+		limit_cone->set_control_point(transformed_control_point.normalized());
+	}
 	update_tangent_radii();
 }
 
